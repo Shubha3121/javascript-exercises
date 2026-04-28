8ffb61785c36b54e46326fe7e07daf183e214a51
Shubha3121 <ammushubha2006@gmail.com>
2026-04-28 12:57:51 +0000

    Add two numbers

--- foundations/02_addNumbers/addNumbers.js
+++ foundations/02_addNumbers/addNumbers.js
@@ -4,10 +4,10 @@ function addNumbers() {
 
   let result;
 
-  result = "a" + "b"; // <------ EDIT THIS LINE
-
+  result = a + b; // <------ EDIT THIS LINE
+  console.log(result); // <------ THIS LINE IS FOR YOU TO SEE THE RESULT IN THE CONSOLE, YOU CAN REMOVE IT IF YOU WANT
   return result;
 }
-
+addNumbers();
 // Do not change this
 module.exports = addNumbers;
