8c9bfc1c2887c03246b8d8d9610b9102a5c686eb
Shubha3121 <ammushubha2006@gmail.com>
2026-04-28 13:05:45 +0000

    fix: update condition in numberChecker (>= 10)

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,10 +1,12 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 10) {
     return true;
   } else {
     return false;
   }
 }
+
+console.log(numberChecker(1237));
 
 // Do not edit below this line
 module.exports = numberChecker;
